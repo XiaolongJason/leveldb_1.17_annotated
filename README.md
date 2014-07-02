4b582f7fcd2f0f40cdebf4b6d3a6ef2f3b3b85fe
Feng,Xiaolong <bjtufengxiaol@gmail.com>
2014-07-02 20:24:01 +0800

    部分已经添加的注释

---
+++ README.md
@@ -0,0 +1,8 @@
+#leveldb 代码注释
+
+本项目是leveldb代码注释。
+
+leveldb Release 1.2 2011-05-16
+
+原始源码来自：https://code.google.com/p/leveldb/
+
---
+++ util/arena.h
@@ -0,0 +1,69 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#ifndef STORAGE_LEVELDB_UTIL_ARENA_H_
+#define STORAGE_LEVELDB_UTIL_ARENA_H_
+
+#include <vector>
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
+
+namespace leveldb {
+
+class Arena {
+ public:
+  Arena();
+  ~Arena();
+
+  // Return a pointer to a newly allocated memory block of "bytes" bytes.
+  char* Allocate(size_t bytes);
+
+  // Allocate memory with the normal alignment guarantees provided by malloc
+  char* AllocateAligned(size_t bytes);
+
+  // Returns an estimate of the total memory usage of data allocated
+  // by the arena (including space allocated but not yet used for user
+  // allocations).
+  size_t MemoryUsage() const {
+    return blocks_memory_ + blocks_.capacity() * sizeof(char*);
+  }
+
+ private:
+  char* AllocateFallback(size_t bytes);
+  char* AllocateNewBlock(size_t block_bytes);
+
+  // Allocation state
+  char* alloc_ptr_;   //当前空闲block地址
+  size_t alloc_bytes_remaining_;  //当前空闲的block剩余内存大小
+
+  // Array of new[] allocated memory blocks
+  std::vector<char*> blocks_; //已经申请的block的地址集合
+
+  // Bytes of memory in blocks allocated so far
+  size_t blocks_memory_;   //已经申请的所有block的内存大小
+
+  // No copying allowed
+  Arena(const Arena&);
+  void operator=(const Arena&);
+};
+
+inline char* Arena::Allocate(size_t bytes) {
+  // The semantics of what to return are a bit messy if we allow
+  // 0-byte allocations, so we disallow them here (we don't need
+  // them for our internal use).
+  assert(bytes > 0);
+  if (bytes <= alloc_bytes_remaining_) {
+	//检查当前的block是否可以分配，如果可以，直接分配
+    char* result = alloc_ptr_;
+    alloc_ptr_ += bytes;
+    alloc_bytes_remaining_ -= bytes;
+    return result;
+  }
+  return AllocateFallback(bytes);
+}
+
+}  // namespace leveldb
+
+#endif  // STORAGE_LEVELDB_UTIL_ARENA_H_
